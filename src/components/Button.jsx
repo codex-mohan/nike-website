ba5b411d1b2b22efec6785476df3ba26ff0ccaf7
codex-mohan <codexmohan@gmail.com>
2024-07-29 00:12:54 +0530

    fixed broken icon on buttons

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,8 @@
-const Button = ({label, iconUrl}) => {
+const Button = ({label, iconURL}) => {
   return (
     <button className='flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red'>
         {label}
-        <img src={iconUrl} alt={label} className="m-2 rounded-full w-5 h-5"/>
+        <img src={iconURL} alt={label} className="m-2 rounded-full w-5 h-5"/>
     </button>
   )
 }
